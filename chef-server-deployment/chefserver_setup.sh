338ea882efa18c5b733d21c382a079277dda8ede
pennyliangliping <lianglip@gmail.com>
2015-07-16 10:55:07 +0800

    modify script to create home folder
    modify script to create home folder

--- chef-server-deployment/chefserver_setup.sh
+++ chef-server-deployment/chefserver_setup.sh
@@ -1,1 +1,7 @@
-mkdir ~/dddddd
+#!/bin/bash
+# setup chef server and chef workstation
+
+mkdir /home/auto/
+mkdir /home/auto/test
+# change the FQDN for the ubuntu server before installing chef server
+
