9fc2a7f0a1e60458b4f8844b8f6858e84edfe7c3
pennyliangliping <lianglip@gmail.com>
2015-07-17 10:28:18 +0800

    upload the version pass test v0.1
    upload the version pass test

--- chef-server-deployment/chefserver_setup.sh
+++ chef-server-deployment/chefserver_setup.sh
@@ -23,148 +23,148 @@ if [ ! -e /home/auto ]; then
 fi
 cd /home/auto
 
-## ensure that the VM name is the same with the beginning of fqdn in Azure template
-#hostname=`hostname`
-#chef_fqdn="${hostname}.eastasia.cloudapp.azure.com"
-## change the FQDN for the ubuntu server before installing chef server
-#sudo echo "127.0.1.1 ${chef_fqdn} ${hostname}" >> /etc/hosts
-#sudo service hostname restart
-#res=`hostname -f`
-#if [ $res != $chef_fqdn ]; then
-#    echo "changing FQDN fail!"
-#    exit 1
-#fi
-#
-## download chef server
-#wget $chef_server_url
-#if [ $? != 0 ]; then
-#    echo "download chef server fail!"
-#    exit 2
-#fi
-#
-## install chef server
-#sudo dpkg -i $chef_server_deb
-#if [ $? != 0 ]; then
-#    echo "dpkg install chef server fail!"
-#    exit 3
-#fi
-#rm $chef_server_deb
-#
-## config chef server
-#sudo chef-server-ctl reconfigure
-#if [ $? != 0 ]; then
-#    echo "reconfigure chef server fail!"
-#    exit 4
-#fi
-#
-## install opscode-manage
-#sudo chef-server-ctl install opscode-manage
-#if [ $? != 0 ]; then
-#    echo "opscode-manage install fail!"
-#    exit 5
-#fi
-#
-## config opscode-manage
-#sudo opscode-manage-ctl reconfigure
-#if [ $? != 0 ]; then
-#    echo "opscode-manage reconfigure fail!"
-#    exit 6
-#fi
-#
-## create a new administor user
-#chef-server-ctl user-create $chef_admin_user sosse sosse auto@auto.com $chef_damin_pw --filename $chef_admin_pem
-#if [ $? != 0 ]; then
-#    echo "create chef server admin fail!"
-#    exit 6
-#fi
-#
-## create a new organization
-#chef-server-ctl org-create $chef_org_name "sosse, Inc." --association_user $chef_admin_user --filename $chef_org_pem
-#if [ $? != 0 ]; then
-#    echo "create chef server organization fail!"
-#    exit 7
-#fi
-#
-############################################################
-##                setup for chef workstation               #
-############################################################
-## install all dependent packages
-#sudo apt-get -y update
-#sudo apt-get install -y git ruby1.9.1-full ruby make g++ zlib1g-dev
-#
-## download chef client
-#wget $chef_client_url
-#if [ $? != 0 ]; then
-#    echo "download chef client fail!"
-#    exit 8
-#fi
-#
-## install chef client
-#sudo dpkg -i $chef_client_deb
-#if [ $? != 0 ]; then
-#    echo "dpkg install chef client fail!"
-#    exit 9
-#fi
-#rm $chef_client_url
-#
-## clone chef-repo
-#git clone https://github.com/chef/chef-repo.git
-#if [ ! -e ./chef-repo ]; then
-#    echo "clone chef-repo fail!"
-#    exit 10
-#fi
-#
-## copy <user>.pem <org>.pem knife.rb to .chef 
-#mkdir ./chef-repo/.chef
-#
-#knife_config="# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
-#
-#current_dir = File.dirname(__FILE__)
-#log_level                :info
-#log_location             STDOUT
-#node_name                \"${chef_admin_user}\"
-#client_key               \"#{current_dir}/${chef_admin_user}.pem\"
-#validation_client_name   \"${chef_org_name}-validator\"
-#validation_key           \"#{current_dir}/${org}-validator.pem\"
-#chef_server_url          \"https://${chef_fqdn}/organizations/${chef_org_name}\"
-#cookbook_path            [\"#{current_dir}/../cookbooks\"]"
-#
-#echo "$knife_config" > knife.rb
-#
-#mv $chef_admin_pem ./chef-repo/.chef
-#mv $chef_org_pem ./chef-repo/.chef
-#mv knife.rb ./chef-repo/.chef
-#cd ./chef-repo
-#knife ssl fetch
-#
-#res=`knife user list`
-#if [ $res != $chef_admin_user ]; then
-#    echo "knife ssl fetch fail!"
-#    exit 11
-#fi
-#
-## download chef dk
-#wget $chef_dk_url
-#if [ $? != 0 ]; then
-#    echo "download chef dk fail!"
-#    exit 12
-#fi
-#
-## install chef client
-#sudo dpkg -i $chef_dk_deb
-#if [ $? != 0 ]; then
-#    echo "dpkg install chef dk fail!"
-#    exit 13
-#fi
-#rm $chef_dk_deb
-#
-## update the bash env for chef
-#`chef shell-init bash`
-#
-## install knife winodws plugin
-#sudo gem install knife-windows
-#if [ $? != 0 ]; then
-#    echo "install knife-winodows plugin fail!"
-#    exit 14
-#fi
-#
+# ensure that the VM name is the same with the beginning of fqdn in Azure template
+hostname=`hostname`
+chef_fqdn="${hostname}.eastasia.cloudapp.azure.com"
+# change the FQDN for the ubuntu server before installing chef server
+sudo echo "127.0.1.1 ${chef_fqdn} ${hostname}" >> /etc/hosts
+sudo service hostname restart
+res=`hostname -f`
+if [ $res != $chef_fqdn ]; then
+    echo "changing FQDN fail!"
+    exit 1
+fi
+
+# download chef server
+wget $chef_server_url
+if [ $? != 0 ]; then
+    echo "download chef server fail!"
+    exit 2
+fi
+
+# install chef server
+sudo dpkg -i $chef_server_deb
+if [ $? != 0 ]; then
+    echo "dpkg install chef server fail!"
+    exit 3
+fi
+rm $chef_server_deb
+
+# config chef server
+sudo chef-server-ctl reconfigure
+if [ $? != 0 ]; then
+    echo "reconfigure chef server fail!"
+    exit 4
+fi
+
+# install opscode-manage
+sudo chef-server-ctl install opscode-manage
+if [ $? != 0 ]; then
+    echo "opscode-manage install fail!"
+    exit 5
+fi
+
+# config opscode-manage
+sudo opscode-manage-ctl reconfigure
+if [ $? != 0 ]; then
+    echo "opscode-manage reconfigure fail!"
+    exit 6
+fi
+
+# create a new administor user
+chef-server-ctl user-create $chef_admin_user sosse sosse auto@auto.com $chef_damin_pw --filename $chef_admin_pem
+if [ $? != 0 ]; then
+    echo "create chef server admin fail!"
+    exit 6
+fi
+
+# create a new organization
+chef-server-ctl org-create $chef_org_name "sosse, Inc." --association_user $chef_admin_user --filename $chef_org_pem
+if [ $? != 0 ]; then
+    echo "create chef server organization fail!"
+    exit 7
+fi
+
+###########################################################
+#                setup for chef workstation               #
+###########################################################
+# install all dependent packages
+sudo apt-get -y update
+sudo apt-get install -y git ruby1.9.1-full ruby make g++ zlib1g-dev
+
+# download chef client
+wget $chef_client_url
+if [ $? != 0 ]; then
+    echo "download chef client fail!"
+    exit 8
+fi
+
+# install chef client
+sudo dpkg -i $chef_client_deb
+if [ $? != 0 ]; then
+    echo "dpkg install chef client fail!"
+    exit 9
+fi
+rm $chef_client_url
+
+# clone chef-repo
+git clone https://github.com/chef/chef-repo.git
+if [ ! -e ./chef-repo ]; then
+    echo "clone chef-repo fail!"
+    exit 10
+fi
+
+# copy <user>.pem <org>.pem knife.rb to .chef 
+mkdir ./chef-repo/.chef
+
+knife_config="# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                \"${chef_admin_user}\"
+client_key               \"#{current_dir}/${chef_admin_user}.pem\"
+validation_client_name   \"${chef_org_name}-validator\"
+validation_key           \"#{current_dir}/${org}-validator.pem\"
+chef_server_url          \"https://${chef_fqdn}/organizations/${chef_org_name}\"
+cookbook_path            [\"#{current_dir}/../cookbooks\"]"
+
+echo "$knife_config" > knife.rb
+
+mv $chef_admin_pem ./chef-repo/.chef
+mv $chef_org_pem ./chef-repo/.chef
+mv knife.rb ./chef-repo/.chef
+cd ./chef-repo
+knife ssl fetch
+
+res=`knife user list`
+if [ $res != $chef_admin_user ]; then
+    echo "knife ssl fetch fail!"
+    exit 11
+fi
+
+# download chef dk
+wget $chef_dk_url
+if [ $? != 0 ]; then
+    echo "download chef dk fail!"
+    exit 12
+fi
+
+# install chef client
+sudo dpkg -i $chef_dk_deb
+if [ $? != 0 ]; then
+    echo "dpkg install chef dk fail!"
+    exit 13
+fi
+rm $chef_dk_deb
+
+# update the bash env for chef
+`chef shell-init bash`
+
+# install knife winodws plugin
+sudo gem install knife-windows
+if [ $? != 0 ]; then
+    echo "install knife-winodows plugin fail!"
+    exit 14
+fi
+
