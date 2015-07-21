d4a57c9ef84e6c2cb7c23e9641b804b381bd50b4
pennyliangliping <lianglip@gmail.com>
2015-07-21 08:43:49 +0800

    change ps1.zip for dsc re-try
    change ps1.zip for dsc re-try
    change public network to private network

--- chef-client-deployment-windows/EnableWinRM.ps1
+++ chef-client-deployment-windows/EnableWinRM.ps1
@@ -1,5 +1,11 @@
-cmd.exe /c "winrm quickconfig -force"
-cmd.exe /c 'winrm set winrm/config/client/auth @{Basic="true"}'
-cmd.exe /c 'winrm set winrm/config/service/auth @{Basic="true"}'
-cmd.exe /c 'winrm set winrm/config/service @{AllowUnencrypted="true"}'
-
+cmd.exe /c "winrm quickconfig -force" >> C:\log.txt
+echo "-----------------------" >> C:\log.txt
+cmd.exe /c 'winrm set winrm/config/client/auth @{Basic="true"}' >> C:\log.txt
+echo "-----------------------" >> C:\log.txt
+cmd.exe /c 'winrm set winrm/config/service/auth @{Basic="true"}' >> C:\log.txt
+echo "-----------------------" >> C:\log.txt
+cmd.exe /c 'winrm set winrm/config/service @{AllowUnencrypted="true"}' >> C:\log.txt
+echo "-----------------------" >> C:\log.txt
+$res = Get-NetConnectionProfile
+$res >> C:\log.txt
+Set-NetConnectionProfile -InterfaceIndex $res.InterfaceIndex -NetworkCategory Private
